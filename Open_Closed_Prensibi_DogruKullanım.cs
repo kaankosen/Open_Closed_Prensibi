515529df05f8477433e995ebb4d1376ac66b879f
Kaan Kösen <121513474+kaankosen@users.noreply.github.com>
2023-03-29 09:46:15 +0300

    Initial commit

---
+++ Open_Closed_Prensibi_DogruKullanım.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Open_Closed_Prensibi
+{
+    public class Musteri
+    {
+        public int MusteriID { get; set; }
+        public string Name { get; set; }
+        public UyelikTipi Uyelik { get; set; }
+    }
+
+    public abstract class UyelikTipi
+    {
+        public abstract decimal Hesapla(decimal urunFiyatı);
+
+    }
+
+    public class StandartUyelikTipi : UyelikTipi
+    {
+        public override decimal Hesapla(decimal urunFiyatı)
+        {
+            return urunFiyatı;
+        }
+
+
+    }
+    public class PremiumUyelikTipi : UyelikTipi
+    {
+        public override decimal Hesapla(decimal urunFiyatı)
+        {
+            return urunFiyatı * (1 - 0.1M);
+        }
+
+
+    }
+
+    public class SiparisOnay
+    {
+        public decimal FiyatHesapla(decimal fiyat, Musteri musteri)
+        {
+            return musteri.Uyelik.Hesapla(fiyat);
+        }
+    }
+}
---
+++ Open_Closed_Prensibi_YanlısKullanım.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum UyelikTipleri { Standart, Premium }
+
+public class Musteri
+{
+    public int MusteriID { get; set; }
+    public string MusteriName { get; set; }
+    public UyelikTipleri Uyelik { get; set; }
+
+}
+
+public class SiparisOnay
+{
+    public decimal FiyatHesaplama(decimal fiyat, Musteri musteri)
+    {
+        decimal sonuc = 0;
+        switch (musteri.Uyelik)
+        {
+            case UyelikTipleri.Standart:
+                sonuc = fiyat;
+                break;
+            case UyelikTipleri.Premium:
+                sonuc = fiyat * (1 - 0.1M);
+                break;
+        }
+        return sonuc;
+    }
+}
